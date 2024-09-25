c54fea962897651533040923cb90c27c8197c5dd
Lucas Vinicius Hartmann <lucas.hartmann@gmail.com>
2024-09-25 16:45:14 -0300

    Make width dynamic to fit extra piece.

--- hanoi.h
+++ hanoi.h
@@ -2,7 +2,7 @@
 
 #include <stdbool.h>
 
-#define HANOI_PIECES 4
+#define HANOI_PIECES 5
 #define HANOI_ROWS (HANOI_PIECES+1)
 #define HANOI_TOWER_WIDTH 10
 #define HANOI_TOWER_GAP 1
@@ -27,17 +27,31 @@ typedef struct hanoi_context_s {
     uint8_t towers[3][HANOI_ROWS];
 } hanoi_context_t;
 
+unsigned hanoi_widest_piece(hanoi_context_t *ctx, int pin) {
+    return ctx->towers[pin][0];
+}
+
+unsigned hanoi_tower_width(hanoi_context_t *ctx, int pin) {
+    unsigned W = hanoi_widest_piece(ctx,pin);
+    if (W < HANOI_PIECES-1)
+        W = HANOI_PIECES-1;
+    return 2*W+1;
+}
+
 void hanoi_print_row_pin(hanoi_context_t *ctx, unsigned row, unsigned pin) {
     unsigned piece = ctx->towers[pin][row];
+    unsigned W = hanoi_widest_piece(ctx, pin);
+    if (W < HANOI_PIECES-1)
+        W = HANOI_PIECES-1;
 
     if (piece) {
-        hanoi_putc(' ', HANOI_TOWER_WIDTH/2 - piece - 1);
-        hanoi_putc('X',                   2 + piece * 2);
-        hanoi_putc(' ', HANOI_TOWER_WIDTH/2 - piece - 1);
+        hanoi_putc(' ', W - piece);
+        hanoi_putc('X', 1 + piece * 2);
+        hanoi_putc(' ', W - piece);
     } else {
-        hanoi_putc(' ', HANOI_TOWER_WIDTH/2 - 1);
-        hanoi_putc('|',                       2);
-        hanoi_putc(' ', HANOI_TOWER_WIDTH/2 - 1);
+        hanoi_putc(' ', W);
+        hanoi_putc('|', 1);
+        hanoi_putc(' ', W);
     }
 }
 
@@ -56,7 +70,12 @@ void hanoi_print_pins(hanoi_context_t *ctx) {
     for (int row = HANOI_ROWS; row--;)
         hanoi_print_row(ctx, row);
 
-    hanoi_putc('=', 3*HANOI_TOWER_WIDTH + 2*HANOI_TOWER_GAP);
+    unsigned W = 2*HANOI_TOWER_GAP
+        + hanoi_tower_width(ctx, 0)
+        + hanoi_tower_width(ctx, 1)
+        + hanoi_tower_width(ctx, 2);
+
+    hanoi_putc('=', W);
     hanoi_putc('\n', 1);
 }
 
@@ -77,8 +96,12 @@ bool hanoi_place(hanoi_context_t *ctx, unsigned pin, unsigned piece) {
     return false;
 }
 
-void hanoi_randomize(hanoi_context_t *ctx) {
+void hanoi_clean(hanoi_context_t *ctx) {
     memset(ctx->towers, 0, sizeof(ctx->towers));
+}
+
+void hanoi_randomize(hanoi_context_t *ctx) {
+    hanoi_clean(ctx);
 
     for (int i=0; i<HANOI_PIECES; ++i) {
         int pin = rand()%3;
@@ -90,6 +113,16 @@ void hanoi_randomize(hanoi_context_t *ctx) {
 
 void hanoi_main(uint8_t *VRAM) {
     hanoi_context_t ctx;
+
+    hanoi_clean(&ctx);
+    for (int i=0; i<HANOI_PIECES; ++i) {
+        int pin = i%3;
+        int piece = HANOI_PIECES-i;
+
+        hanoi_place(&ctx, pin, piece);
+    }
+    hanoi_print_pins(&ctx);
+    Delay_Ms(5000);
 
     for (int i=0; i<60; i++) {
         hanoi_randomize(&ctx);
